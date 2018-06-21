7165146b196039bb0cdb73c3b8d8039cc34b196b
dkopec91 <dkopec91@gmail.com>
2018-06-22 00:07:26 +0200

    Added mouse hook functions and grouped WindowsApi.cs functions in code regions.

--- GridIt/WindowsApi.cs
+++ GridIt/WindowsApi.cs
@@ -1,24 +1,15 @@
 ﻿using System;
 using System.Runtime.InteropServices;
-using System.Windows;
 using System.Windows.Forms;
 
 namespace GridIt
 {
     public static class WindowsApi
     {
+        #region Window management
         private const int GWL_EXSTYLE = -20;
         private const int WS_EX_TRANSPARENT = 0x20;
 
-        public const int WM_HOTKEY_MSG_ID = 0x0312;
-        public enum KeyModifiers
-        {
-            MOD_ALT = 0x0001,
-            MOD_CONTROL = 0x0002,
-            MOD_NOREPEAT = 0x4000,
-            MOD_SHIFT = 0x0004
-        }
-        
         [DllImport("user32.dll")]
         private static extern IntPtr GetDesktopWindow();
 
@@ -31,12 +22,6 @@ namespace GridIt
         [DllImport("user32.dll")]
         private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
-        [DllImport("user32.dll")]
-        public static extern bool RegisterHotKey(IntPtr hWnd, int id, KeyModifiers fsModifiers, Keys vk);
-
-        [DllImport("user32.dll")]
-        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
-
         public static void SetAsOverlay(Form form)
         {
             int exstyle = GetWindowLong(form.Handle, GWL_EXSTYLE);
@@ -48,5 +33,37 @@ namespace GridIt
             SetParent(hwndf, hwndParent);
             form.TopMost = true;
         }
+        #endregion
+
+        #region Hotkeys
+        public const int WM_HOTKEY_MSG_ID = 0x0312;
+        public enum KeyModifiers
+        {
+            MOD_ALT = 0x0001,
+            MOD_CONTROL = 0x0002,
+            MOD_NOREPEAT = 0x4000,
+            MOD_SHIFT = 0x0004
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, KeyModifiers fsModifiers, Keys vk);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        #endregion
+
+        #region Mouse Hook
+        public const int WH_MOUSE_LL = 14;
+        public delegate int HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+
+        [DllImport("user32.dll")]
+        public static extern int CallNextHookEx(int idHook, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+        #endregion
     }
 }
